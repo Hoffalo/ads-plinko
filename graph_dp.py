f3e3b530f7197141e7b39de5ef1169e3526d72e8
jinin-alj <jaoaljayyous.ieu2024@student.ie.edu>
2025-11-29 22:51:12 +0100

    quick modifications

--- graph_dp.py
+++ graph_dp.py
@@ -8,15 +8,15 @@ def node_for_slot(column):
     return ("slot", column)
 
 def build_graph(board_model):
-    neighbors_dictionary = {}
-    start_nodes_dictionary = {}
+    neighbors = {}
+    start_nodes = {}
 
     peg_list = board_model.get_pegs()
 
     for peg_row, peg_column in peg_list:
         peg_node = node_for_peg(peg_row, peg_column)
-        if peg_node not in neighbors_dictionary:
-            neighbors_dictionary[peg_node] = []
+        if peg_node not in neighbors:
+            neighbors[peg_node] = []
 
         left_child, right_child = board_model.get_children_of_peg(peg_row, peg_column)
 
@@ -26,26 +26,25 @@ def build_graph(board_model):
             if isinstance(child, int):
                 slot_column = child
                 slot_node = node_for_slot(slot_column)
-                if slot_node not in neighbors_dictionary:
-                    neighbors_dictionary[slot_node] = []
-                neighbors_dictionary[peg_node].append((slot_node, 0.5))
+                if slot_node not in neighbors:
+                    neighbors[slot_node] = []
+                neighbors[peg_node].append((slot_node, 0.5))
             else:
                 child_row, child_column = child
                 child_peg_node = node_for_peg(child_row, child_column)
-                if child_peg_node not in neighbors_dictionary:
-                    neighbors_dictionary[child_peg_node] = []
-                neighbors_dictionary[peg_node].append((child_peg_node, 0.5))
+                if child_peg_node not in neighbors:
+                    neighbors[child_peg_node] = []
+                neighbors[peg_node].append((child_peg_node, 0.5))
 
     number_of_columns = board_model.number_of_columns
 
     for column in range(number_of_columns):
         start_node = first_node_for_column(board_model, column)
-        start_nodes_dictionary[column] = start_node
-        if start_node is not None and start_node not in neighbors_dictionary:
-            neighbors_dictionary[start_node] = []
+        start_nodes[column] = start_node
+        if start_node is not None and start_node not in neighbors:
+            neighbors[start_node] = []
 
-    return neighbors_dictionary, start_nodes_dictionary
-
+    return neighbors, start_nodes
 def first_node_for_column(board_model, column):
     row = 0
     while row < board_model.number_of_rows and board_model.is_empty(row, column):
@@ -57,26 +56,25 @@ def first_node_for_column(board_model, column):
     return node_for_slot(column)
 
 def compute_expected_values(board_model):
-    neighbors_dictionary, start_nodes_dictionary = build_graph(board_model)
-    expected_value_dictionary = {}
+    neighbors, start_nodes = build_graph(board_model)
+    expected_value = {}
 
     def expected_value_for_node(node):
-        if node in expected_value_dictionary:
-            return expected_value_dictionary[node]
-
+        if node in expected_value:
+            return expected_value[node]
         kind = node[0]
 
         if kind == "slot":
             slot_column = node[1]
             value = float(board_model.get_slot_score_at_column(slot_column))
-            expected_value_dictionary[node] = value
+            expected_value[node] = value
             return value
 
-        neighbor_list = neighbors_dictionary.get(node, [])
+        neighbor_list = neighbors.get(node, [])
 
         if not neighbor_list:
             value = 0.0
-            expected_value_dictionary[node] = value
+            expected_value[node] = value
             return value
 
         total = 0.0
@@ -84,12 +82,12 @@ def compute_expected_values(board_model):
             child_value = expected_value_for_node(child_node)
             total = total + probability * child_value
 
-        expected_value_dictionary[node] = total
+        expected_value[node] = total
         return total
 
     result_list = []
     for column in range(board_model.number_of_columns):
-        start_node = start_nodes_dictionary.get(column)
+        start_node = start_nodes.get(column)
         if start_node is None:
             result_list.append(0.0)
         else:
