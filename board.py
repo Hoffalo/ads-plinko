630def66646a9165edeba178fd95d0c285c466bc
danadabbahh <danadabbah28@gmail.com>
2025-11-29 22:21:57 +0100

    ValueErrors

--- board.py
+++ board.py
@@ -15,14 +15,14 @@ class BoardModel:
 
     def get_cell(self, row, column):
         if not self.in_bounds(row, column):
-            raise Exception("Out of bounds")
+            raise ValueError("Out of bounds")
         return self.grid[row][column]
 
     def set_cell(self, row, column, value):
         if not self.in_bounds(row, column):
-            raise Exception("Out of bounds")
+            raise ValueError("Out of bounds")
         if value != EMPTY and value != PEG:
-            raise Exception("Invalid value")
+            raise ValueError("Invalid value")
         self.grid[row][column] = value
 
     def is_peg(self, row, column):
@@ -44,13 +44,13 @@ class BoardModel:
 
     def get_slot_score_at_column(self, column):
         if not (0 <= column < self.number_of_columns):
-            raise Exception("Invalid column")
+            raise ValueError("Invalid column")
         return self.slot_scores[column]
     
 #children can be either another peg node, a slot, or outside the board (None)
     def get_children_of_peg(self, row, column):
         if not self.is_peg(row, column):
-            raise Exception("There is no peg at this position")
+            raise ValueError("There is no peg at this position")
         # where the ball goes if it bounces left   
         left_child = self.child_direction(row, column, -1)
         #where the ball goes if it bounces right
